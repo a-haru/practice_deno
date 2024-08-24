936b123106c1c13397cf483cad3afa3af711306c
a-haru <47050280+a-haru@users.noreply.github.com>
2024-08-24 16:36:01 +0900

    fix readFileからreadTextFileに変更

--- steps/02/01.ts
+++ steps/02/01.ts
@@ -1,3 +1,9 @@
+/**
+ * @see https://docs.deno.com/runtime/tutorials/read_write_files/#reading-a-text-file
+ * @see https://qiita.com/access3151fq/items/233bd3d72f62ad684fa1
+ */
+
 const filepath = new URL("./files/01-sample.txt", import.meta.url);
-const content = await Deno.readFile(filepath);
-console.log(new TextDecoder().decode(content));
+const content = await Deno.readTextFile(filepath);
+
+console.log(content);
